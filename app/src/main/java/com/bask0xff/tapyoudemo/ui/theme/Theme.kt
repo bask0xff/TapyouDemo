e9f72e2bd9f506f09d82044f009863b368c625bf
sergey <sergey.iddqd@yandex.ru>
2025-05-22 09:45:47 +0200

    delete unused import

--- app/src/main/java/com/bask0xff/tapyoudemo/ui/theme/Theme.kt
+++ app/src/main/java/com/bask0xff/tapyoudemo/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.bask0xff.tapyoudemo.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
