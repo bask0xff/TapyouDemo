38029c45475a0e79be5268ddb8122bda0291ec7a
sergey <sergey.iddqd@yandex.ru>
2025-05-22 12:41:25 +0200

    enable view binding

--- app/src/main/java/com/bask0xff/tapyoudemo/presentation/main/MainActivity.kt
+++ app/src/main/java/com/bask0xff/tapyoudemo/presentation/main/MainActivity.kt
@@ -3,13 +3,16 @@ package com.bask0xff.tapyoudemo.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.bask0xff.tapyoudemo.R
+import com.bask0xff.tapyoudemo.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
